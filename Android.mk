00a6039fa2ac5d76538c79552a8311aa9d8726bc
haran <harangovindan@gmail.com>
2019-04-10 06:28:51 +0200

    msm8953-common: Add support for Rosy (Redmi 5)
    Change-Id: I0d189dfc686a0ab9531cb89ea910a52f1e46948e

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter mido tissot,$(TARGET_DEVICE)),)
+ifneq ($(filter mido rosy tissot,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
