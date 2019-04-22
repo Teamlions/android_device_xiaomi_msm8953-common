b495fe3f874c0b35840f6f2118ba198d4624dec3
muralivijay <muralidharan9845@gmail.com>
2019-04-23 05:00:15 +0530

    msm8953-common: Drop lineageHW
    * useless for rosy
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,10 +167,6 @@ HWUI_COMPILE_FOR_PERF := true
 TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
-# Lineage Hardware
-BOARD_HARDWARE_CLASS += \
-    $(VENDOR_PATH)/lineagehw
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
