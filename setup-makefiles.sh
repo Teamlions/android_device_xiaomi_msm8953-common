e63253420978a61fec44d9c85f283dbf7b51ce35
muralivijay <muralidharan9845@gmail.com>
2019-04-26 01:58:42 +0530

    msm8953-common: Drop support mido and tissot
    * Our common tree now based SDM450
    * Update copyright year
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "mido rosy tissot"
+write_headers "rosy"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
