22a2a40c290be041ad857900f0a2420b78c159cc
muralivijay <muralidharan9845@gmail.com>
2019-05-13 16:07:16 +0000

    msm8953-common: Drop doze mode

--- msm8953.mk
+++ msm8953.mk
@@ -138,10 +138,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
