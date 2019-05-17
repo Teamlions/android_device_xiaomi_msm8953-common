51b94f2589e3778501584a4645993b68102257ad
muralivijay <muralidharan9845@gmail.com>
2019-05-17 17:55:36 +0000

    msm8953-common: Nitrogenos bringup
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -18,14 +18,6 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    Bluetooth \
-    Settings \
-    SettingsProvider \
-    SystemUI \
-    framework-res \
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -242,10 +234,6 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msm8953
-
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
